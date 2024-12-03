9fd3b8abeb89435ba5768fab75bdbac9f3efa708
Paul Zhang <paulzh@umich.edu>
2024-12-03 15:47:36 -0500

    CNN model first update

---
+++ data.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from sklearn.preprocessing import LabelEncoder
+
+class NewsDataset(Dataset):
+    def __init__(self, feature_path, label_path, label_col):
+
+        self.features = np.load(feature_path)
+        self.labels = pd.read_csv(label_path)[label_col]
+        self.label_encoder = LabelEncoder()
+        self.encoded_labels = self.label_encoder.fit_transform(self.labels)
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        feature = torch.tensor(self.features[idx], dtype=torch.float32)
+        label = torch.tensor(self.encoded_labels[idx], dtype=torch.long)
+        return feature, label
+
+def get_loader(train_feature_path, train_label_path, test_feature_path, test_label_path, batch_size, valid_size, col = "Stance"):
+    train_valid_data = NewsDataset(train_feature_path, train_label_path, col)
+    test_data = NewsDataset(test_feature_path, test_label_path, col)
+    train_data, valid_data = random_split(train_valid_data, [1 - valid_size, valid_size])
+    return DataLoader(train_data, batch_size = batch_size, shuffle = True), DataLoader(valid_data, batch_size = batch_size, shuffle = True), DataLoader(test_data, batch_size=batch_size, shuffle = False)
